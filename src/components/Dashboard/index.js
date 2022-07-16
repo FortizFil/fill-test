fc3af4bd211f43e759100b56edf135d453c96d27
Vitalii Filchenko <vfilchenko95@gmail.com>
2022-07-16 13:50:00 +0300

    Add parse link request

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+
 import Box from "@mui/material/Box";
+
+import { axiosConfig } from "utils/axiosConfig";
+import { PARSE_LINK } from "constans/API";
 
 import SelectTerm from "components/SelectTerm";
 import SelectBrand from "components/SelectBrand";
@@ -11,10 +15,57 @@ const Dashboard = () => {
   const [brand, setBrand] = useState("");
   const [style, setStyle] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
+  const a = location.pathname.split("/");
 
   let url = `${term.length > 0 ? "/s-" + term : term}${
     brand.length > 0 ? "/b-" + brand : brand
   }${style.length > 0 ? "/st-" + style : style}`;
+
+  const getParam = (arr, param) => {
+    if (param === "term") {
+      const currentParam = arr
+        .filter((el) => el[0] === "s" && el[1] === "-")[0]
+        ?.split("");
+      currentParam?.splice(0, 2);
+      return currentParam ? currentParam.join("") : "";
+    } else if (param === "brand") {
+      const currentParam = arr
+        .filter((el) => el[0] === "b" && el[1] === "-")[0]
+        ?.split("");
+      currentParam?.splice(0, 2);
+      return currentParam ? currentParam.join("") : "";
+    } else {
+      const currentParam = arr
+        .filter((el) => el[0] === "s" && el[1] === "t")[0]
+        ?.split("");
+      currentParam?.splice(0, 3);
+      return currentParam ? currentParam.join("") : "";
+    }
+  };
+
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      let link = location.pathname.split("/");
+      link.shift();
+      axiosConfig({
+        method: "get",
+        url: `${PARSE_LINK}?brand_slug=${getParam(
+          a,
+          "brand"
+        )}&service_slug=${getParam(a, "term")}&style_slug=${getParam(
+          a,
+          "style"
+        )}`,
+      })
+        .then((resp) => {
+          setTerm(resp.data.service?.slug || "");
+          setBrand(resp.data.brand?.slug || "");
+          setStyle(resp.data.style?.slug || "");
+        })
+        .catch((error) => console.log(error));
+    }
+  }, []);
 
   useEffect(() => {
     navigate(url);
--- src/constans/API.js
+++ src/constans/API.js
@@ -1,5 +1,5 @@
-export const BACKEND_URL = 'https://onboarding.art-code.team/'
-export const GET_TERMS = 'api/test/v1/search/terms'
-export const GET_BRANDS = 'api/test/v1/search/brands_terms'
-export const GET_STYLES = 'api/test/v1/search/styles'
-export const PARSE_LINK = 'api​/test​/v1​/search​/parse_link'
+export const BACKEND_URL = "https://onboarding.art-code.team/";
+export const GET_TERMS = "api/test/v1/search/terms";
+export const GET_BRANDS = "api/test/v1/search/brands_terms";
+export const GET_STYLES = "api/test/v1/search/styles";
+export const PARSE_LINK = "api/test/v1/search/parse_link";
