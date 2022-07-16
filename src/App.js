d4a6b907cee6eb96f7db99e626537116bc1d8c9e
Vitalii Filchenko <vfilchenko95@gmail.com>
2022-07-16 11:05:01 +0300

    Add dashboard component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import { useEffect } from 'react';
-
-import {axiosConfig} from 'utils/axiosConfig'
-import {GET_TERMS} from 'constans/API'
+import Dashboard from 'components/Dashboard'
 
 function App() {
 
-  useEffect(()=>{
-    axiosConfig({
-      method:'get',
-      url:GET_TERMS
-    }).then((resp)=>console.log(resp))
-  },[])
 
   return (
-    <div className="App">
-      <h1>Hell fggo</h1>
-    </div>
+    <Dashboard/>
   );
 }
 
